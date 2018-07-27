37677dc62a775986d0e74eba5f04893e6d0b10c6
h-huss <37378879+h-huss@users.noreply.github.com>
2018-07-27 11:15:03 +0200

    Rename main class

--- src/main/java/de/viadee/sonarIssueScoring/SonarIssueScoringApplication.java
+++ src/main/java/de/viadee/sonarIssueScoring/SonarIssueScoringApplication.java
@@ -1,21 +1,23 @@
 package de.viadee.sonarIssueScoring;
 
-import com.fasterxml.jackson.datatype.guava.GuavaModule;
-import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jackson.Jackson2ObjectMapperBuilderCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import com.fasterxml.jackson.datatype.guava.GuavaModule;
+import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
+
 @SpringBootApplication
 @Configuration
-public class WebStart {
-    @Bean public Jackson2ObjectMapperBuilderCustomizer configureObjectMapper() {
+public class SonarIssueScoringApplication {
+    @Bean
+    public Jackson2ObjectMapperBuilderCustomizer configureObjectMapper() {
         return builder -> builder.modules(new GuavaModule(), new Jdk8Module()).failOnUnknownProperties(true);
     }
 
     public static void main(String[] args) {
-        SpringApplication.run(WebStart.class, args);
+        SpringApplication.run(SonarIssueScoringApplication.class, args);
     }
 }
