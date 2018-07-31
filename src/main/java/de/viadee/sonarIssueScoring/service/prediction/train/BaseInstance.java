6e5913fa8c6374f16b772a713e47bc899b94f909
h-huss <37378879+h-huss@users.noreply.github.com>
2018-07-31 10:54:41 +0200

    Make an internal method deprecated

--- src/main/java/de/viadee/sonarIssueScoring/service/prediction/train/BaseInstance.java
+++ src/main/java/de/viadee/sonarIssueScoring/service/prediction/train/BaseInstance.java
@@ -1,18 +1,20 @@
 package de.viadee.sonarIssueScoring.service.prediction.train;
 
+import java.nio.file.Path;
+import java.time.DayOfWeek;
+import java.util.Map;
+
+import org.immutables.value.Value.Default;
+import org.immutables.value.Value.Immutable;
+
 import com.fasterxml.jackson.annotation.JsonAnyGetter;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.ImmutableMap.Builder;
-import de.viadee.sonarIssueScoring.misc.ImmutableStyle;
-import org.immutables.value.Value.Default;
-import org.immutables.value.Value.Immutable;
 
-import java.nio.file.Path;
-import java.time.DayOfWeek;
-import java.util.Map;
+import de.viadee.sonarIssueScoring.misc.ImmutableStyle;
 
 /**
  * A specific instance of data the ML-Algorithm is trained on
@@ -74,7 +76,9 @@ public abstract class BaseInstance {
 
     //This construct of ignoring map fields and adding them back is required due to jackson-csv inability to handle maps
     //See also CsvConverter.java
-    @JsonAnyGetter public Map<String, Integer> _flattenedMapsForJackson() {
+    @Deprecated
+    @JsonAnyGetter
+    public Map<String, Integer> _flattenedMapsForJackson() {
         Builder<String, Integer> builder = ImmutableMap.builder();
         commitAge().forEach((age, val) -> builder.put("commitAge." + age, val));
         commitAgePackage().forEach((age, val) -> builder.put("commitAgePackage." + age, val));
