1e75c82a1547a0659830e6628a2aee4bf9b830c5
h-huss <37378879+h-huss@users.noreply.github.com>
2018-07-31 10:42:52 +0200

    Make the actual type ofd the metric clearer

--- src/main/java/de/viadee/sonarIssueScoring/service/prediction/BasePredictionResult.java
+++ src/main/java/de/viadee/sonarIssueScoring/service/prediction/BasePredictionResult.java
@@ -1,17 +1,19 @@
 package de.viadee.sonarIssueScoring.service.prediction;
 
-import com.fasterxml.jackson.databind.annotation.JsonSerialize;
-import com.google.common.collect.ImmutableMap;
-import de.viadee.sonarIssueScoring.misc.ImmutableStyle;
+import java.nio.file.Path;
+
 import org.immutables.value.Value.Immutable;
 
-import java.nio.file.Path;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.google.common.collect.ImmutableMap;
+
+import de.viadee.sonarIssueScoring.misc.ImmutableStyle;
 
 @JsonSerialize
 @Immutable
 @ImmutableStyle
 public abstract class BasePredictionResult {
-    public abstract ModelMetrics metrics();
+    public abstract ModelMetrics validationMetrics();
 
     public abstract ImmutableMap<Path, FileInformation> results();
 }
